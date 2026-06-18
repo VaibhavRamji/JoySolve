304bd391f257ab4972d38dbbd00e8333acff772b
Vaibhav <vaibhav.r.iyengar@gmail.com>
2026-06-18 22:44:12 +0530

    Corrected comments with factual information

--- FVM and physics/meshing.py
+++ FVM and physics/meshing.py
@@ -1,8 +1,8 @@
 class Mesh:
     def __init__(self,length,width,height,nx,ny,nz):
-        self.length = length # Length of cell
-        self.width = width # width of cell
-        self.height = height # width of cell
+        self.length = length # Length of domain
+        self.width = width # width of domain
+        self.height = height # width of domain
         self.nx = nx  # number of cells in x direction
         self.ny = ny # number of cells in y direction
         self.nz = nz # number of cells in z direction
