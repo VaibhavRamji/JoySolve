ffbe10cfc9945ba09864d3363a239f1cb25147cf
Vaibhav <vaibhav.r.iyengar@gmail.com>
2026-06-18 22:42:27 +0530

    Created class: Mesh. Created method: __init__

---
+++ FVM and physics/meshing.py
@@ -0,0 +1,8 @@
+class Mesh:
+    def __init__(self,length,width,height,nx,ny,nz):
+        self.length = length # Length of cell
+        self.width = width # width of cell
+        self.height = height # width of cell
+        self.nx = nx  # number of cells in x direction
+        self.ny = ny # number of cells in y direction
+        self.nz = nz # number of cells in z direction
